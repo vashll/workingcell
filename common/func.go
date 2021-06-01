49b4d1602a18bb9dbd2e8a15ad771ef128b22435
Vash <lemonade.liu@gmail.com>
2021-06-01 18:16:49 +0800

    Update func.go

--- common/func.go
+++ common/func.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"fmt"
 	"net"
 	"strings"
 )
@@ -11,11 +10,28 @@ var RemoteIp string
 
 func FetchNetIp() {
 	conn, err := net.Dial("udp", "google.com:80")
+	if err == nil {
+		defer conn.Close()
+		LocalIp = strings.Split(conn.LocalAddr().String(), ":")[0]
+		RemoteIp = strings.Split(conn.LocalAddr().String(), ":")[0]
+	} else {
+		LocalIPv4s()
+	}
+}
+
+func LocalIPv4s() {
+	var ips []string
+	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		fmt.Println(err.Error())
 		return
 	}
-	defer conn.Close()
-	LocalIp = strings.Split(conn.LocalAddr().String(), ":")[0]
-	RemoteIp = strings.Split(conn.LocalAddr().String(), ":")[0]
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ips = append(ips, ipnet.IP.String())
+		}
+	}
+	if len(ips) > 0 {
+		LocalIp = strings.Split(ips[0], ":")[0]
+	}
 }
